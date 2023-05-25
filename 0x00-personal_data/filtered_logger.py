2ab8840e3754d786102b090f77acc5b79eb64f6e
wachira140 <wachiraw424@gmail>
2023-05-25 16:13:39 +0300

    initial commit

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+""" Module filtering logs
+"""
+import os
+import re
+import logging
+import mysql.connector
+from typing import List
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
+def filter_datum(fields, sub, message,sep):
+    '''for fld in fields:
+        pattern = "{}=[\w+,/]*".format(fld) 
+        replacer = '{}={}'.format(fld, sub)
+        message = re.sub(pattern, replacer, message)
+    return message
+    '''
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
+    
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        formats a log record
+        """
+        msg = super(RedactingFormatter, self).format(record)
+        text = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return text
+
+
+def get_logger() -> logging.Logger:
+    '''return a logging.Logger object
+    '''
+    logger = logging.getLogger("user_data")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db():
+    '''connect to a db
+    '''
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    connector = mysql.connect(
+        host = db_host,
+        port=3306,
+        name=db_name,
+        user=db_user,
+        password=db_password
+    )
+    return connector
+
+
+def main() -> None:
+    """Log the information about user records in a table
+    """
+    query_fields = "name, email, phone, ssn, password, ip, last_login, user_agent"
+    columns = query_fields.split(',')
+    query = "SELECT {} FROM users".format(query_fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row)
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
+
+if __name__ == "__main__":
+    main()
