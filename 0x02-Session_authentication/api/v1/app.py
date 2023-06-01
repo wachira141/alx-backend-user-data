d2a3abac68eb6b181bebaa58c590a3e871135b99
wachira140 <wachiraw424@gmail>
2023-06-01 14:10:19 +0300

    Session authentication implementation

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+auth_type = getenv('AUTH_TYPE', None)
+if auth_type:
+    from api.v1.auth.auth import Auth
+    if auth_type == 'auth':
+        auth = Auth()
+    if auth_type == 'basic_auth':
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    if auth_type == 'SessionAuth':
+        from api.v1.auth.session_auth import SessionAuth
+        auth = SessionAuth()
+    if auth_type == 'session_exp_auth':
+        from api.v1.auth.session_exp_auth import SessionExpAuth
+        auth = SessionExpAuth()
+    if auth_type == 'session_db_auth':
+        from api.v1.auth.session_exp_auth import SessionDBAuth
+        auth = SessionDBAuth()
+
+
+@app.before_request
+def handle_before_req():
+    """before request handler
+    """
+    if auth is not None:
+        req_path = request.path
+        paths_list = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+        req_auth = auth.require_auth(req_path, paths_list)
+
+        req_headers = request.headers
+        if req_auth:
+            auth_header = auth.authorization_header(req_headers)
+            auth_session = auth.session_cookie(request)
+
+            if auth_header is None and auth_session is None:
+                abort(401)
+
+            current_user = request.current_user()
+            if current_user is None:
+                abort(403)
+    else:
+        pass
+
+
+@app.errorhandler(401)
+def un_authorized(error) -> str:
+    """Not authorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_route(error) -> str:
+    """forbidden route handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port, debug=True)
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+"""Basic auth
+    inherit form class Auth
+"""
+
+import re
+import base64
+import binascii
+from typing import Tuple, TypeVar
+
+from auth import Auth
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """BasicAuth class that inherits from Auth class
+    """
+
+    def extract_base64_authorization_header(self, authorization_header) -> str:
+        """return a Base64 auth header"""
+        if authorization_header is None \
+                or type(authorization_header) is not str:
+            return None
+        pattern = r'Basic (?P<token>.+)'
+        auth_header = re.fullmatch(pattern, authorization_header.strip())
+        if auth_header is not None:
+            return auth_header.group('token')
+
+        return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str,
+            ) -> str:
+        """Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> Tuple[str]:
+        """extract user credentials from the base64 decoded value"""
+        if decoded_base64_authorization_header is None \
+                or type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+
+        pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+        match_str = re.fullmatch(pattern,
+                                 decoded_base64_authorization_header.strip())
+
+        if match_str is None:
+            return None, None
+
+        user = match_str.group('user')
+        password = match_str.group('password')
+        return user, password
+
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """return an instance of a user based on the email passed"""
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the user from a request.
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
