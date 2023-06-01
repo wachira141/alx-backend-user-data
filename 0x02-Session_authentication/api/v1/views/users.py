d8415a2f0acb7a21f2e44449390a1305861cab01
wachira140 <wachiraw424@gmail>
2023-06-01 14:34:57 +0300

    change /views/user.py condition stmnt

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -26,17 +26,17 @@ def view_one_user(user_id: str = None) -> str:
       - 404 if the User ID doesn't exist
     """
     current_usr = request.current_user
-    if user_id == 'me' and current_usr is None:
+    if user_id is None:
         abort(404)
 
-    if user_id is None:
+    if user_id == 'me' and current_usr is None:
         abort(404)
 
     user = User.get(user_id)
     if user is None:
         abort(404)
 
-    if current_usr == 'me' and current_usr is not None:
+    if user_id == 'me' and current_usr is not None:
         return jsonify(request.current_user.to_json())
     return jsonify(user.to_json())
 
