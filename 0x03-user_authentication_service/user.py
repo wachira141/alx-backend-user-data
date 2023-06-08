d83bfb9349b2ba91fbef05d70e562b97f6db89e1
wachira141 <wachiraw75@gmail.com>
2023-06-08 11:20:58 -0700

    remove typing on users model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,8 +12,8 @@ Base = declarative_base()
 class User(Base):
     """class model to create a user"""
     __tablename__ = "users"
-    id: int = Column(INTEGER, primary_key=True, autoincrement=True)
-    email: str = Column(String(250), nullable=False)
-    hashed_password: str = Column(String(250), nullable=False)
-    session_id: str = Column(String(250), nullable=True)
-    reset_token: str = Column(String(250), nullable=True)
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
